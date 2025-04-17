599316be2c8d0ae3d17f2af93d94c6bec0e7831f
healrabbit <alphasorell@gmail.com>
2025-04-17 12:25:34 -0400

    community quiz
    just need to add some more visual details

---
+++ quiz.js
@@ -0,0 +1,96 @@
+document.addEventListener("DOMContentLoaded",function(){
+
+
+let sun = document.querySelectorAll('.sunlight');
+for (var i = sun.length-1; i>=0;i--){
+sun[i].addEventListener("click",function (){ 
+let sunval = this.value;
+localStorage.setItem("sunlight", sunval);
+sun.forEach(element => {
+element.classList.toggle("selected",false);
+  });
+this.classList.toggle("selected",true);
+}); 
+
+}
+
+let dog = document.querySelectorAll('.dog');
+for (var i = dog.length-1; i>=0;i--){
+dog[i].addEventListener("click",function (){ 
+
+let dogval = this.value;
+localStorage.setItem("dog", dogval);
+dog.forEach(element => {
+    element.classList.toggle("selected",false);
+      });
+    this.classList.toggle("selected",true);
+    });
+}
+
+let entertain = document.querySelectorAll('.party');
+for (var i = entertain.length-1; i>=0;i--){
+entertain[i].addEventListener("click",function (){ 
+
+let partyval = this.value;
+localStorage.setItem("entertain", partyval);
+entertain.forEach(element => {
+    element.classList.toggle("selected",false);
+      });
+    this.classList.toggle("selected",true);
+    });
+}
+
+let plants = document.querySelectorAll('.plants');
+for (var i = plants.length-1; i>=0;i--){
+plants[i].addEventListener("click",function (){ 
+
+let plantval = this.value;
+localStorage.setItem("plants", plantval);
+plants.forEach(element => {
+    element.classList.toggle("selected",false);
+      });
+    this.classList.toggle("selected",true);
+    });
+}
+
+let pollution = document.querySelectorAll('.pollution');
+for (var i = pollution.length-1; i>=0;i--){
+pollution[i].addEventListener("click",function (){ 
+
+let polval = this.value;
+localStorage.setItem("pollution", polval);
+pollution.forEach(element => {
+    element.classList.toggle("selected",false);
+      });
+    this.classList.toggle("selected",true);
+    });
+}
+
+let mosquito = document.querySelectorAll('.mosquito');
+for (var i = mosquito.length-1; i>=0;i--){
+mosquito[i].addEventListener("click",function (){ 
+
+let mosquitoval = this.value;
+localStorage.setItem("mosquito", mosquitoval);
+mosquito.forEach(element => {
+    element.classList.toggle("selected",false);
+      });
+    this.classList.toggle("selected",true);
+    });
+}
+let rat = document.querySelectorAll('.rat');
+for (var i = rat.length-1; i>=0;i--){
+rat[i].addEventListener("click",function (){ 
+
+let ratval = this.value;
+localStorage.setItem("rat", ratval);
+rat.forEach(element => {
+    element.classList.toggle("selected",false);
+      });
+    this.classList.toggle("selected",true);
+    });
+}
+
+
+});
+
---
+++ quizresults.js
@@ -0,0 +1,203 @@
+document.addEventListener("DOMContentLoaded",function(){
+
+// div list 
+
+let fsbush = document.getElementById('fsbush');
+let psbush = document.getElementById('psbush');
+let sbush =  document.getElementById('sbush');
+let fsflower = document.getElementById('fsflower');
+let psflower = document.getElementById('psflower');
+let sflower =  document.getElementById('sflower');
+let fsherb = document.getElementById('fsherb');
+let psherb = document.getElementById('psherb');
+let sherb =  document.getElementById('sherb');
+let dogpark = document.getElementById('dogpark');
+let party = document.getElementById('dinnerparty');
+let polluted= document.getElementById('pollution');
+let soiltest = document.getElementById('soiltest');
+let mosquito= document.getElementById('mosquito');
+let rat = document.getElementById('rat');
+let non = document.getElementById('non');
+
+//DOGS
+if (localStorage.getItem("dog") === "hasdog"){
+   dogpark.style.display = 'block';
+   fsbush.style.display = 'none';
+   psbush.style.display = 'none';
+   sbush.style.display = 'none';
+   fsherb.style.display = 'none';
+   psherb.style.display = 'none';
+   sherb.style.display = 'none';
+   fsflower.style.display = 'none';
+   psflower.style.display = 'none';
+   sflower.style.display = 'none';
+   soiltest.style.display = 'none';
+   polluted.style.display = 'none';
+   party.style.display = 'none';
+   non.style.display = 'none';
+}
+else if (localStorage.getItem("dog") === "nodog"){
+    dogpark.style.display = 'none';
+
+//ENTERTAINING SPACE 
+if (localStorage.getItem("entertain") === "entertain"){
+    fsbush.style.display = 'none';
+    psbush.style.display = 'none';
+    sbush.style.display = 'none';
+    fsherb.style.display = 'none';
+    psherb.style.display = 'none';
+    sherb.style.display = 'none';   
+     fsflower.style.display = 'none';
+    psflower.style.display = 'none';
+    sflower.style.display = 'none';
+   dogpark.style.display = 'none';
+   party.style.display = 'block';
+   soiltest.style.display = 'none';
+   polluted.style.display = 'none';
+   non.style.display = 'none';
+
+}
+else if (localStorage.getItem("entertain") === "noparty"){
+    party.style.display = 'none';
+///PLANTS 
+if(localStorage.getItem("plants") !== "noplants"){
+    non.style.display = 'none';
+    //POLLUTION
+    if(localStorage.getItem("pollution") === "pol"){
+        soiltest.style.display = 'none';
+        polluted.style.display = 'block';
+    }
+    if(localStorage.getItem("pollution") === "notest"){
+        soiltest.style.display = 'block';
+        polluted.style.display = 'none';
+    }
+    if(localStorage.getItem("pollution") === "nopol"){
+        soiltest.style.display = 'none';
+        polluted.style.display = 'none';
+    }
+    //FLOWERS
+
+if(localStorage.getItem("plants") === "flowering"){
+    fsbush.style.display = 'none';
+    psbush.style.display = 'none';
+    sbush.style.display = 'none';
+    fsherb.style.display = 'none';
+    psherb.style.display = 'none';
+    sherb.style.display = 'none';
+    if(localStorage.getItem("sunlight") === "partial"){
+        fsflower.style.display = 'none';
+        psflower.style.display = 'block';
+        sflower.style.display = 'none';
+        }
+        else if(localStorage.getItem("sunlight") === "full"){
+            fsflower.style.display = 'block';
+            psflower.style.display = 'none';
+            sflower.style.display = 'none';
+            }
+            
+        else if(localStorage.getItem("sunlight") === "little"){
+                fsflower.style.display = 'none';
+                psflower.style.display = 'none';
+                sflower.style.display = 'block';
+                }
+            }
+//BUSHES
+if(localStorage.getItem("plants") === "bushes"){
+    fsflower.style.display = 'none';
+    psflower.style.display = 'none';
+    sflower.style.display = 'none';
+    fsherb.style.display = 'none';
+    psherb.style.display = 'none';
+    sherb.style.display = 'none';
+    if(localStorage.getItem("sunlight") === "partial"){
+        fsbush.style.display = 'none';
+        psbush.style.display = 'block';
+        sbush.style.display = 'none';
+        }
+        else if(localStorage.getItem("sunlight") === "full"){
+            fsbush.style.display = 'block';
+            psbush.style.display = 'none';
+            sbush.style.display = 'none';
+            }
+            
+        else if(localStorage.getItem("sunlight") === "little"){
+                fsbush.style.display = 'none';
+                psbush.style.display = 'none';
+                sbush.style.display = 'block';
+                }
+            }
+//HERBS
+
+if(localStorage.getItem("plants") === "edible"){
+    fsbush.style.display = 'none';
+    psbush.style.display = 'none';
+    sbush.style.display = 'none';
+    fsflower.style.display = 'none';
+    psflower.style.display = 'none';
+    sflower.style.display = 'none';
+    if(localStorage.getItem("sunlight") === "partial"){
+        fsherb.style.display = 'none';
+        psherb.style.display = 'block';
+        sherb.style.display = 'none';
+        }
+        else if(localStorage.getItem("sunlight") === "full"){
+            fsherb.style.display = 'block';
+            psherb.style.display = 'none';
+            sherb.style.display = 'none';
+            }
+            
+        else if(localStorage.getItem("sunlight") === "little"){
+                fsherb.style.display = 'none';
+                psherb.style.display = 'none';
+                sherb.style.display = 'block';
+                }
+            }
+            }
+      
+        }
+        }
+        if(localStorage.getItem("dog") === "nodog" && localStorage.getItem("plants") === "noplants"
+         && localStorage.getItem("entertain") === "noparty"){
+            non.style.display = 'block';
+            dogpark.style.display = 'none';
+            fsbush.style.display = 'none';
+            psbush.style.display = 'none';
+            sbush.style.display = 'none';
+            fsherb.style.display = 'none';
+            psherb.style.display = 'none';
+            sherb.style.display = 'none';
+            fsflower.style.display = 'none';
+            psflower.style.display = 'none';
+            sflower.style.display = 'none';
+            soiltest.style.display = 'none';
+            polluted.style.display = 'none';
+            party.style.display = 'none';
+        }
+
+
+        //PESTS
+        if(localStorage.getItem("mosquito") === "nomosq"){
+            mosquito.style.display = 'none';
+ 
+        }
+        if(localStorage.getItem("rat") === "norat"){
+            rat.style.display = 'none';
+        
+        }
+        if(localStorage.getItem("mosquito") === "hasmosq"){
+            mosquito.style.display = 'block';
+         
+        }
+      
+      
+        if(localStorage.getItem("rat") === "hasrat"){
+            rat.style.display = 'block';
+        }
+ document.querySelector("#re").addEventListener("click",function(){
+localStorage.clear();
+
+
+ });
+});
+
+console.log(localStorage.getItem("dog") )
